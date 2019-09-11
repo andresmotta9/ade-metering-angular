cd0d412da9a2d32aa83cede5379a48ea2eb939cf
German Andres Sanchez Motta <german.sanchez@decameron.com>
2019-09-11 16:28:40 -0500

    a

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ade-metering-angular'));
+app.use(express.static('./dist/ade-metering'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname, '/dist/ade-metering-angular/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/ade-metering/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
